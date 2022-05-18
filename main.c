b728841209dcf315e7f89fdf51a12c88f5a0ed24
Adereti Adebobola <emmanueladebobola132@gmail.com>
2022-05-18 21:01:26 +0100

    commit

--- main.c
+++ main.c
@@ -1,73 +1,87 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
- * free_data - frees data structure
- *
- * @datash: data structure
- * Return: no return
+ * execute - executes the command
+ * @cmd: command to run
+ * Return: 0 on success1 -1 if cmd is exit and 1 on any other error
  */
-void free_data(data_shell *datash)
+int execute(char **cmd)
 {
-	unsigned int i;
 
-	for (i = 0; datash->_environ[i]; i++)
+	pid_t child_pid;
+	int status;
+
+	if (strncmp("exit", cmd[0], 4) == 0)
+		return (-1);
+
+	child_pid = fork();
+
+	if (child_pid == -1)
 	{
-		free(datash->_environ[i]);
+		perror("Error");
+		return (1);
 	}
+	else if (child_pid == 0)
+	{
+		if (execve(cmd[0], cmd, NULL) == -1)
+		{
+			perror("Error");
+			exit(-1);
+		}
+	}
+	else
+		wait(&status);
 
-	free(datash->_environ);
-	free(datash->pid);
+	return (0);
 }
 
+
 /**
- * set_data - Initialize data structure
- *
- * @datash: data structure
- * @av: argument vector
- * Return: no return
+ * main - main simple shell
+ * @argc: number of arguments
+ * @argv: list of command line arguments
+ * Return: Always 0, -1 on error.
  */
-void set_data(data_shell *datash, char **av)
+
+int main(int argc, char **argv)
 {
-	unsigned int i;
 
-	datash->av = av;
-	datash->input = NULL;
-	datash->args = NULL;
-	datash->status = 0;
-	datash->counter = 1;
+	int response;
+	char **tokens;
+	size_t bufsize = BUFSIZ;
+	int isPipe = 0;
+	char *buffer;
 
-	for (i = 0; environ[i]; i++)
-		;
-
-	datash->_environ = malloc(sizeof(char *) * (i + 1));
-
-	for (i = 0; environ[i]; i++)
+	if (argc >= 2)
 	{
-		datash->_environ[i] = _strdup(environ[i]);
+		/*TODO: Handle cases where there is no argument, only the command*/
+		if (execve(argv[1], argv, NULL) == -1)
+		{
+			perror("Error");
+			exit(-1);
+		}
+		return (0);
 	}
 
-	datash->_environ[i] = NULL;
-	datash->pid = aux_itoa(getpid());
-}
+	buffer = (char *)malloc(bufsize * sizeof(char));
+	if (buffer == NULL)
+	{
+		perror("Unable to allocate buffer");
+		exit(1);
+	}
 
-/**
- * main - Entry point
- *
- * @ac: argument count
- * @av: argument vector
- *
- * Return: 0 on success.
- */
-int main(int ac, char **av)
-{
-	data_shell datash;
-	(void) ac;
+	do {
+		if (isatty(fileno(stdin)))
+		{
+			isPipe = 1;
+			_puts("cisfun#: ");
+		}
 
-	signal(SIGINT, get_sigint);
-	set_data(&datash, av);
-	shell_loop(&datash);
-	free_data(&datash);
-	if (datash.status < 0)
-		return (255);
-	return (datash.status);
+		getline(&buffer, &bufsize, stdin);
+		buffer[_strlen(buffer) - 1] = '\0';
+		tokens = stringToTokens(buffer);
+		response = execute(tokens);
+	} while (isPipe && response != -1);
+
+	return (0);
 }
